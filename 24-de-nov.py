df89fc8a59aeab1e8823284fb93da9589b68a9c1
Vinicio Borja <75345399+Vinici0@users.noreply.github.com>
2023-11-24 13:32:46 -0500

    Update 24-de-nov.py

--- 24-de-nov.py
+++ 24-de-nov.py
@@ -44,18 +44,18 @@ class SyncCategory(models.Model):
             cursor14.execute(categories_odoo14_query)
             categories_odoo14 = cursor14.fetchall()
 
-            for category_odoo11 in categories_odoo11:
+            for category_odoo11 in categories_odoo11: 
                 exist = False
                 # Recorrer las categorías de Odoo 14 para verificar si la categoría ya existe
                 for category_odoo14 in categories_odoo14:
                     # print(category_odoo14)
                     if category_odoo11['id'] == category_odoo14['id']:
                         exist = True
-                        parent_path = self.convert_parent_path(category_odoo11['parent_left'],
-                                                               category_odoo11['parent_right'])
-                        # Verificar y actualizar claves foráneas (no se proporciona el código correspondiente)
-                        # ...
-
+                        parent_path = SyncCategory.convert_parent_path(category_odoo11['parent_id'])
+                        path_new = f"{parent_path}/{category_odoo11['id']}/" if parent_path else f"{category_odoo11['id']}/"
+                        print(path_new)
+                        
+    
                         if category_odoo11['parent_id']:
                             print('Verificando claves foráneas')
                             self.foreign_key_validate(category_odoo11)
@@ -70,9 +70,10 @@ class SyncCategory(models.Model):
                                     create_uid = {category_odoo11['create_uid']},
                                     write_date = '{category_odoo11['write_date']}',
                                     create_date = '{category_odoo11['create_date']}',
-                                    parent_path = '{parent_path}',
+                                    parent_path = '{path_new}',
                                     complete_name = '{category_odoo11['complete_name']}'
                                     WHERE id = {category_odoo11['id']}"""
+
                         cursor14.execute(query)
                         print('Actualizando categoría (solo name)')
                 # Mensaje en el log
@@ -80,12 +81,11 @@ class SyncCategory(models.Model):
                 if not exist:
                     print('Creando categoría')
                     # Convertir la representación de la jerarquía
-                    parent_path = self.convert_parent_path(category_odoo11['parent_left'],
-                                                           category_odoo11['parent_right'])
+                    parent_path = SyncCategory.convert_parent_path(category_odoo11['parent_id'], category_odoo11['id'])
 
                     # Insertar solo el campo 'name' en Odoo 14
-                    query = f"""INSERT INTO product_category (id, name, parent_id, write_uid, create_uid, write_date, create_date, parent_path, complete_name)
-                                VALUES ({category_odoo11['id']}, '{category_odoo11['name']}' , {category_odoo11['parent_id']}, {category_odoo11['write_uid']}, {category_odoo11['create_uid']}, '{category_odoo11['write_date']}','{category_odoo11['create_date']}'), '{parent_path}'"""
+                    query = f"""INSERT INTO product_category (id, name, write_uid, create_uid, write_date, create_date, parent_path, complete_name)
+                                VALUES ({category_odoo11['id']}, '{category_odoo11['name']}', {category_odoo11['write_uid']}, {category_odoo11['create_uid']}, '{category_odoo11['write_date']}', '{category_odoo11['create_date']}', '{parent_path}', '{category_odoo11['complete_name']}')"""
 
                     cursor14.execute(query)
 
@@ -94,15 +94,36 @@ class SyncCategory(models.Model):
         except Exception as e:
             _logger.error("Error: %s", e)
 
-
     @staticmethod
-    def convert_parent_path(parent_left, parent_right):
-        # Lógica de conversión de parent_left y parent_right a parent_path
-        # Implementa aquí tu lógica específica para convertir la jerarquía
+    def convert_parent_path(parent_id):
+        conn11 = psycopg2.connect(
+            host=config['gserp_db_host'],
+            database=config['gserp_db_name'],
+            user=config['gserp_db_user'],
+            password=config['gserp_db_password'])
 
-        # Ejemplo simple: convertir parent_left y parent_right a una cadena "1/2/3/"
-        return "/".join(map(str, range(parent_left, parent_right + 1))) + "/"
+        cursor11 = conn11.cursor(cursor_factory=RealDictCursor)
 
+        parent_path = []
+
+        while parent_id:
+            # Fetch the parent category information
+            cursor11.execute(f"SELECT id, parent_id, name FROM product_category WHERE id = {parent_id}")
+            parent_category = cursor11.fetchone()
+
+            if parent_category:
+                # Insert the parent category id into the list
+                parent_path.insert(0, str(parent_category['id']))
+
+                # Update parent_id to continue the loop
+                parent_id = parent_category['parent_id']
+            else:
+                # Break the loop if the parent category is not found
+                break
+
+        conn11.close()
+
+        return '/'.join(parent_path)
 
     @staticmethod
     def foreign_key_validate(category_odoo11):
@@ -110,7 +131,5 @@ class SyncCategory(models.Model):
         category_odoo11['write_uid'] = 'null' if not category_odoo11['write_uid'] else category_odoo11['write_uid']
         category_odoo11['create_uid'] = 'null' if not category_odoo11['create_uid'] else category_odoo11['create_uid']
         category_odoo11['write_date'] = 'null' if not category_odoo11['write_date'] else category_odoo11['write_date']
-        category_odoo11['create_date'] = 'null' if not category_odoo11['create_date'] else category_odoo11['create_date']
-
-
-
+        category_odoo11['create_date'] = 'null' if not category_odoo11['create_date'] else category_odoo11[
+            'create_date']
